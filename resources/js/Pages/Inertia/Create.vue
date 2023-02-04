fc35fc0fa5d8667e2961e014367a4080580b9973
Yudai35 <yuusongben14@gmail.com>
2023-02-04 17:07:57 +0900

    Vue.js Inertia CRUD実装

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+
+const form = reactive({
+    title: null,
+    content: null
+})
+
+const submitFunction = () => {
+    Inertia.post('/inertia', form)
+}
+
+
+</script>
+
+
+<template>
+    <p>クリエイト</p>
+    <form @submit.prevent="submitFunction">
+        <input name="title" type="text" v-model="form.title"> <br>
+        <input name="content" type="text" v-model="form.content">
+        <button>送信</button>
+    </form>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue'
+
+const newTitle = ref('');
+const newContent = ref('');
+
+</script>
+
+<template>
+    <p>InertiaTestです. </p> <br>
+    <a href="/">aタグ経由</a> <br>
+    <Link href="/">Link経由</Link> <br>
+    <Link :href="route('inertia.index')">名前つきrouteの確認</Link> <br>
+    <Link :href="route('inertia.show', {id : 1} )">ルートパラメーターの確認</Link>
+
+    <div class="mb-8">
+        <input type="text" name="newTitle" v-model="newTitle"> <br>
+        <input type="text" name="newContent" v-model="newContent"> <br>
+        <Link as="button" method="post" :href="route('inertia.store')" 
+        :data="{ 
+            title : newTitle,
+            content : newContent
+         }">DB保存</Link>
+    </div>
+
+    
+
+
+</template>
